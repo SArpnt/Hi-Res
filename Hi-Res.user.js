59276f595fef14c40ab0e43abc03cad0ac564a55
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-15 11:16:10 -0400

    5.4.0
    bitmap cache fixes, smaller code, and game code commented out

--- Hi-Res.user.js
+++ Hi-Res.user.js
@@ -2,7 +2,7 @@
 // @name         Hi-Res
 // @namespace    http://tampermonkey.net/
 // @run-at       document-start
-// @version      5.3.3
+// @version      5.4.0
 // @description  no more blocky blur
 // @author       SArpnt
 // @match        https://boxcritters.com/play/
@@ -20,58 +20,73 @@
 
 (function () {
 	'use strict';
-	cardboard.on('login', function () {
-		world.stage.cache = joinFunction(world.stage.cache, world.stage.hUpdate);
-		window.addEventListener('resize', world.stage.hUpdate);
-		world.stage.hUpdate();
-	});
-	cardboard.on('runScriptShowGame', function () {
-		showGame = joinFunction(showGame, function () {
-			world.stage.hXPx = $('#game').width();
-			world.stage.hWidthPx = -world.stage.hXPx;
-			world.stage.hUpdate();
-			world.stage.room.focus();
-		});
-	});
 	cardboard.on('worldStageCreated', function () {
-		let e = world.stage;
-		e.hX = e.x;
-		e.hY = e.y;
-		e.hXPx = 0;
-		e.hYPx = 0;
+		let s = world.stage;
+		s.hX = s.x;
+		s.hY = s.y;
+		s.hXPx = 0;
+		s.hYPx = 0;
 
-		e.hWidth = e.width;
-		e.hHeight = e.height;
-		e.hWidthPx = 0;
-		e.hHeightPx = 0;
+		s.hWidth = s.width;
+		s.hHeight = s.height;
+		s.hWidthPx = 0;
+		s.hHeightPx = 0;
 
-		e.hScale = 1;
+		s.hScale = 1;
 
-		e.hCanvasWidth = e.width;
-		e.hCanvasHeight = e.height;
+		s.hCanvasWidth = s.width;
+		s.hCanvasHeight = s.height;
 
-		e.hiRes = true;
-		e.hUpdate = function () {
-			let stage = world.stage;
-			let canvas = stage.canvas;
+		s.hiRes = true;
+		s.hUpdate = function () {
+			let canvas = s.canvas;
 
-			canvas.height = stage.hCanvasHeight;
-			canvas.width = stage.hCanvasWidth;
+			canvas.height = s.hCanvasHeight;
+			canvas.width = s.hCanvasWidth;
 
 			let scale = canvas.offsetWidth * window.devicePixelRatio / canvas.width;
-			stage.hCanvasScale = scale;
+			s.hCanvasScale = scale;
 
 			let temp = canvas.offsetWidth * window.devicePixelRatio;
 			canvas.height = canvas.offsetHeight * window.devicePixelRatio;
 			canvas.width = temp;
 
-			stage.x = stage.hXPx + (stage.hX * scale);
-			stage.y = stage.hYPx + (stage.hY * scale);
-			stage.width = stage.hWidth + (stage.hWidthPx / scale);
-			stage.height = stage.hHeight + (stage.hHeightPx / scale);
-			stage.scale = stage.hScale * scale;
+			s.x = s.hXPx + (s.hX * scale);
+			s.y = s.hYPx + (s.hY * scale);
+			s.width = s.hWidth + (s.hWidthPx / scale);
+			s.height = s.hHeight + (s.hHeightPx / scale);
+			s.scale = s.hScale * scale;
 
-			if (stage.bitmapCache) stage.bitmapCache.scale = stage.hScale;
+			if (s.bitmapCache)
+				if (s.bitmapCache.hScale)
+					s.bitmapCache.scale = s.bitmapCache.hScale * scale;
+				else if (s.bitmapCache.hUpdate)
+					s.bitmapCache.hUpdate();
 		};
+		cardboard.on('login', function () {
+			s.cache = joinFunction(s.cache, function (_a, _b, _c, _d, _e, _f, h) {
+				if (h) {
+					if (typeof h == 'function') {
+						s.bitmapCache.hUpdate = h;
+						s.bitmapCache.hUpdate();
+					}
+				} else {
+					s.bitmapCache.hScale = s.bitmapCache.scale;
+					s.bitmapCache.scale = s.bitmapCache.hScale * s.hCanvasScale;
+				}
+			});
+			window.addEventListener('resize', s.hUpdate);
+			s.hUpdate();
+			/*
+			cardboard.on('runScriptShowGame', function () {
+				showGame = joinFunction(showGame, function () {
+					s.hXPx = $('#game').width();
+					world.stage.hWidthPx = -world.stage.hXPx;
+					world.stage.hUpdate();
+					world.stage.room.focus();
+				});
+			});
+			*/
+		});
 	});
 })();
